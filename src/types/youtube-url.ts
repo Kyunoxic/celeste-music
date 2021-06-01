6d1f7d3391e89b75c8131766759ec14364d1f3e4
Cas <37454616+Cas-E@users.noreply.github.com>
2021-06-02 00:18:02 +0200

    Add youtube-url ArgumentType

---
+++ src/types/youtube-url.ts
@@ -0,0 +1,19 @@
+import {Argument, ArgumentType, CommandoClient, CommandoMessage} from 'discord.js-commando';
+import urlParser from 'js-video-url-parser';
+
+export class YoutubeUrlArgumentType extends ArgumentType {
+    constructor(client: CommandoClient) {
+        super(client, 'youtube-url');
+    }
+
+    validate(value: string, msg: CommandoMessage, arg: Argument) {
+        const parsedValue = urlParser.parse(value);
+        return Boolean(parsedValue) && parsedValue?.provider === 'youtube';
+    }
+
+    parse(value: string) {
+        return value;
+    }
+}
+
+module.exports = YoutubeUrlArgumentType;
